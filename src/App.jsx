f0b4b7d6547037d4e09c8633244634b409d8a1d9
Harshita <harshitasaini469@gmail.com>
2023-08-02 21:28:22 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './components/Main';
+
+// link='https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple'
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
